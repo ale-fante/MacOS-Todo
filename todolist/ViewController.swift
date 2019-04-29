0bad3e6e4918e205debfba2d0258a4e1daeec2c1
Alejandra Ledesma <Alejandra.Ledesma@nick.com>
2019-04-29 13:02:26 -0700

    Initial Commit

---
+++ todolist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Ledesma, Alejandra on 4/29/19.
+//  Copyright © 2019 Ledesma, Alejandra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
